88b38d6ac7f8001edd1d465eef385d0ff9593dd6
Joao Gabriel <jg2718@usp.br>
2025-10-31 21:49:34 -0300

    Finalização do Blog - Render

--- meublog/settings.py
+++ meublog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t=4!bnk*vo=_8xucm_zdy$wqn#owd=(h8n(56tf85bfbi_n^ub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition
