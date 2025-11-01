1cfbe7f812d916a1468382688eff9bf486560b86
Joao Gabriel <jg2718@usp.br>
2025-11-01 10:00:32 -0300

    Versão Final 3.0

--- meublog/settings.py
+++ meublog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,9 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'default:' dj_database_url.config(
-            deafult=os.getenv('DATABASE_URL')
-        )
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL')
+        ),
     }
 }
 
